0fd6e44fc64aa344b9f99559f4e3052ae2aac86f
Deryk Egan <deryk.egan@strath.ac.uk>
2013-02-04 15:24:07 +0000

    admin_pages initial implementation
    The foundation for editing pages - currently retrieves all pages that
    can be changed by users.

---
+++ admin_pages.php
@@ -0,0 +1,47 @@
+<?php
+	// import session and header
+	include_once 'libraries/admin_sql.php';
+	include_once 'libraries/user_check.php';
+	include_once 'classes/pageFactory.php';
+	
+	// check is admin, direct to index if not
+	if(!validateUserType("admin")){
+		header("Location: index.php");
+	}
+	
+	// heredoc for page content
+$sitePage = <<<EOT
+	
+	<br />
+	<h1>Page Editing</h1>
+	<h2>Select a page in order to edit its content.</h2>
+	<br />
+    
+EOT;
+	
+	// now generate table
+	$pages = getSitePages();
+	$sitePage = $sitePage . ('<div class="tableContainer"><table class="twoCol">');
+	
+	$size = count($pages);
+	for($i = 0; $i < $size; $i++){
+		
+		if($i % 2){
+			$sitePage = $sitePage . ('<tr class = "odd">' . '<td class = "odd">' . $pages[$i]["Page_title"] . 
+			'<td class = "even">'. $pages[$i]["Page_content"] . '</tr>');
+		}
+		else{
+			$sitePage = $sitePage . ('<tr class = "even">' . '<td class = "odd">' . $pages[$i]["Page_title"] . 
+			'<td class = "even">'. $pages[$i]["Page_content"] . '</tr>');
+		}
+	}
+	$sitePage = $sitePage . ('</table></div>');
+	
+	// create page factory and generate new page
+	$pageFactory = new pageFactory();
+	$page = $pageFactory->makeHFCookiesPage($sitePage);
+	
+	// print page to screen
+	echo($page->getPage());
+	
+?>
